a9292dd5aaa11de131577485b760838c78b7cac0
Alexander Kuzmenko <alex@stablex.ru>
2019-04-17 13:55:47 +0300

    A script for reproducing display issues

---
+++ display/Run.hx
@@ -0,0 +1,45 @@
+package display;
+
+import haxe.macro.Compiler;
+
+using sys.io.File;
+using haxe.Json;
+
+/**
+ * Run with `haxe -x display.Run -D port=XXXXX` from the root of HaxeRepro.
+ * Port is used as an argument for `--connect XXXXX`.
+ * Default port is 60000.
+ */
+class Run {
+	static public function main() {
+		var args = ['--connect', getPort()].concat(readDisplayArgs());
+		var c = Sys.command('haxe', args);
+		trace('Exit code: $c');
+	}
+
+	static function getPort():String {
+		var port = Compiler.getDefine('port');
+		return (port == null ? '60000' : port);
+	}
+
+	static function readDisplayArgs():Array<String> {
+		var rawData = 'display/args.txt'.getContent();
+		rawData = rawData.substring(rawData.indexOf('[') + 1, rawData.lastIndexOf(']'));
+
+		var jsonPos = rawData.indexOf('{');
+		var args = rawData.substr(0, jsonPos).split(',');
+		var display:DisplayJson = rawData.substr(jsonPos).parse();
+		display.params.contents = display.params.file.getContent();
+
+		return args.concat([display.stringify()]);
+	}
+}
+
+typedef DisplayJson = {
+	jsonrpc:String,
+	id:Int,
+	method:String,
+	params:{
+		file:String, contents:String, offset:String
+	}
+}
