401d66694a669f1556754125985ee9135f1e6c38
Jens Fischer <jensfischer95@gmail.com>
2020-01-04 16:31:24 +0100

    reproduce

--- source/Main.hx
+++ source/Main.hx
@@ -1,5 +1,26 @@
+package;
+
+using tink.CoreApi;
+
+@await
 class Main {
 	static function main() {
-		trace("Test");
+		new Main();
+	}
+
+	function new() {
+		doStuff().handle(o -> trace(o));
+	}
+
+	var foo:Int;
+
+	@async function doStuff() {
+		foo = @await gen();
+	}
+
+	function gen():Promise<Int> {
+		var t = Future.trigger();
+		t.trigger(Success(42));
+		return t;
 	}
 }
